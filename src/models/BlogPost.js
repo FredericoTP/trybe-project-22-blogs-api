bb386db02e532b8a7fb908a7ce94ccf2a7975966
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-04-06 15:36:11 -0300

    Feat: Requisito 10

---
+++ src/models/BlogPost.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      content: {
+        type: DataTypes.STRING,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      published: {
+        type: DataTypes.DATE,
+      },
+      updated: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: 'published',
+      updatedAt: 'updated',
+      underscored: true,
+      tableName: 'categories',
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      {
+        foreignKey: 'user_id',
+        as: 'user',
+      }
+    );
+  };
+
+  return BlogPost;
+}
